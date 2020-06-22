878059ac1a72a5eca6eac94462270bf972012847
EP <kjuioqq8@gmail.com>
2020-06-22 17:33:36 +0900

    (2020.06.22) 실습1 - 3

--- 백준알고리즘/백준알고리즘/Problem5543 - unfinished.java
+++ 백준알고리즘/백준알고리즘/Problem5543 - unfinished.java
@@ -2,32 +2,85 @@ package 백준알고리즘;
 
 import java.util.Scanner;
 
-public class Problem2884 {
+public class Problem5543 {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		
-		int H = sc.nextInt();
-		int M = sc.nextInt();
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int minA;
+		int minB;
+		int minC;
+		int min = 0;
 		
-		if(M < 45) {
-			if(H == 0) {
-				H = H + 23;
-				M = 60 - (45 - M);
-			}
-			else {
-				H = H -1;
-				M = 60 - (45 - M);
-			}
+		if(A + a < A + b) {
+			minA = A + a;		
 		}
 		else {
-			H = H;
-			M = M - 45;
+			minA = A + b;
 		}
 		
-		System.out.println(H);
-		System.out.println(M);
+		if(B + a < B + b) {
+			minB = B + a;		
+		}
+		else {
+			minB = B + b;
+		}
+		
+		if(C + a < C + b) {
+			minC = C + a;		
+		}
+		else {
+			minC = C + b;
+		}
+		
+		if(minA != minB && minB != minC && minA != minC) {
+		
+		if(minA < minB) {
+			if(minA < minC) {
+				min = minA;
+			}
+			else if (minC < minA) {
+				min = minC;
+			}
+		}
+		else if (minB < minC) {
+			if(minB < minA) {
+				min = minB;
+			}
+			else if (minA < minB) {
+				min = minA;
+			}
+		}
+		else {		if(minA <= minB) {
+			if(minA <= minC) {
+				min = minA;
+			}
+			else if (minC <= minA) {
+				min = minC;
+			}
+		}
+		else if (minB <= minC) {
+			if(minB <= minA) {
+				min = minB;
+			}
+			else if (minA <= minB) {
+				min = minA;
+			}
+		}
+			
+		}
+
+		}
+		System.out.println(min - 50);
 
 	}
 
+	}
 }
+
