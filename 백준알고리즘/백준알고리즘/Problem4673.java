066f4eebbf12bc67edf46621f7d555894f6f312b
EP <kjuioqq8@gmail.com>
2020-07-05 23:33:50 +0900

    (2020.07.05) commit

--- 백준알고리즘/백준알고리즘/Problem4673.java
+++ 백준알고리즘/백준알고리즘/Problem4673.java
@@ -3,8 +3,35 @@ package 백준알고리즘;
 public class Problem4673 {
 
 	public static void main(String[] args) {
-		/
-
+		
+		
+		int n = (int) Math.random()*10000 + 1;
+		int result = 0;
+		int x = 10;
+		int a = 1;
+		int sum = 0;
+		
+		
+		int f() {
+		while(true) {
+			
+			if(a == 0) {
+				return sum;
+				break;
+			}
+			
+			else {
+			a = n %= x;
+			sum += a;
+			x *= 10;
+			}
+		}
+		}
+		
+		
+		
+		
+		
 	}
 
 }
