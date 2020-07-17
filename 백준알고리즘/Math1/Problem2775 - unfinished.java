9671cc13316e73e8fa940945b1b3542f07e05900
EP <kjuioqq8@gmail.com>
2020-07-17 21:48:19 +0900

    (2020.07.17) 백준 알고리즘 수학1

--- 백준알고리즘/Math1/Problem2775 - unfinished.java
+++ 백준알고리즘/Math1/Problem2775 - unfinished.java
@@ -16,19 +16,40 @@ public class Problem2775 {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		int test = 0;
-
+		int sum = 0;
+		
+		int[] arr = new int[14];
+		
+		for(int i = 1; i <= 14; i++) {
+			arr[i-1] = i;
+		}
+		
+		
+		
+		int[] arr2 = new int[14];
+		
+			for(int i = 1; i <= 14; i++) {
+				for(int j = 1; j <= i; j++) {
+					sum += arr[j-1];
+				}
+				arr2[i] = sum;
+				sum = 0;
+			}
 		
 		
 		while(test < T) {
 			test++;
 			int k = sc.nextInt();
 			int n = sc.nextInt();
-			int result = function(n);
-			int tmp = 0;
+			int result = 0;
+			int sum = 0;
 			
-			for(int i = 1; i < k; i++) {
-				tmp = function(result);
-				result = tmp;
+			for(int i = 0; i < k; i++) {
+				for(int j = 1; j <= n; j++) {
+					sum += function(j);
+					result += sum;
+					sum = 0;
+				}
 			}
 			
 			System.out.println(result);
