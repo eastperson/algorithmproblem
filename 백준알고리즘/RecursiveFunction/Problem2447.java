e3bad62a153a68d3813c2595bf2904b27f2efe3a
EP <kjuioqq8@gmail.com>
2020-08-15 23:26:28 +0900

    (2020.08.15) Baek algorithm recursiveFunction

--- 백준알고리즘/RecursiveFunction/Problem2447.java
+++ 백준알고리즘/RecursiveFunction/Problem2447.java
@@ -2,26 +2,29 @@ package RecursiveFunction;
 
 public class Problem2447 {
 	
-	public static void p() {
-		System.out.println("*");
+	public static void rec(String input) {
+		System.out.print(output(input));
 	}
 	
-	
-	public static void f() {
-		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 3; j++) {
-				if(i == 1 && j == 1) {p();}
-				else {p();}
-			}
-			System.out.println();
-		}
+	public static String output(String input) {
+		String first = input + input + input;
+		String second = input + space(input) + input;
+		String third = first;
+		return first + "\n" + second + "\n" + third;
 	}
 	
+	public static String space(String input) {
+		String tmp = "";
+		for(int i = 0; i < input.length(); i++)
+			tmp += " ";
+		String space = input.replace(input, tmp);
+		return space;
+	}
 	
 	
 	public static void main(String[] args) {
-		f("*");
-
+		System.out.println(output(output("*")));
+	
 	}
 
 }
