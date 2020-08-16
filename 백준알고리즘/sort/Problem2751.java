728616690be03c5a046c2c9a7f0c7a91c95f7f2f
EP <kjuioqq8@gmail.com>
2020-08-16 20:55:20 +0900

    (2020.08.16) Baek algorithm sort

---
+++ 백준알고리즘/sort/Problem2751.java
@@ -0,0 +1,24 @@
+package sort;
+
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Problem2751 {
+	public static void main(String[] args) {
+		Set set = new TreeSet();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 0; set.size() < n; i++) {
+			set.add(new Integer(sc.nextInt()));
+		}
+		
+		Iterator it = set.iterator();
+		
+		while (it.hasNext()) {
+			Object obj = it.next();
+			System.out.println(obj);
+		}
+	}
+}
