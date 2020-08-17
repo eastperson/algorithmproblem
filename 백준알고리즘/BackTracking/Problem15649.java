66c16592ec9fa46b41770375922df59c7e9391c5
EP <kjuioqq8@gmail.com>
2020-08-18 07:48:56 +0900

    (2020.08.18) Baek algorithm sort

--- 백준알고리즘/BackTracking/Problem15649.java
+++ 백준알고리즘/BackTracking/Problem15649.java
@@ -8,12 +8,9 @@ public class Problem15649 {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int m = sc.nextInt();
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < m; j++) {
-				System.out.print();
-			}
+		
 			
-		}
+		
 
 	}
 
