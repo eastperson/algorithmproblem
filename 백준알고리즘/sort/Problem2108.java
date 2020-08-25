62d89daaac947c72d46ad7459307337546711340
EP <kjuioqq8@gmail.com>
2020-08-25 22:55:11 +0900

    (2020.08.25) Baek algorithm sort

--- 백준알고리즘/sort/Problem2108.java
+++ 백준알고리즘/sort/Problem2108.java
@@ -1,28 +1,44 @@
 package sort;
 
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
 
 public class Problem2108 {
 	
 	public static void main(String[] args) {
-		Set set = new TreeSet();
+		// N은 홀수
 		Scanner sc = new Scanner(System.in);
+		List<Integer> list = new ArrayList<>();
+		int sum = 0;
 		int n = sc.nextInt();
-		for(int i = 0; set.size() < n; i++) {
-			set.add(new Integer(sc.nextInt()));
+		for(int i = 0; i < n; i ++) {
+			list.add(sc.nextInt());
 		}
 		
+		Collections.sort(list);
+		
+		for(int i = 0; i < list.size(); i++) {
+			sum += list.get(i);
+		}
+		
+		// 산술평균 : N개의 수들의 합을 N으로 나눈 값
+		System.out.println(sum/list.size());
 		
 		
-//		Iterator it = set.iterator();
-//		
-//		while (it.hasNext()) {
-//			Object obj = it.next();
-//			System.out.println(obj);
-//		}
+		// 중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위차하는 값
+		int a = (int) (list.size()/2);
+		System.out.println(list.get(a));
+		
+		// 최빈값 : N개의 수들 중 가장 많이 나타나는 값
+		Collections.
+		
+		
+		// 범위 : N개의 수들 중 최댓값과 최솟값의 차이
+		System.out.println(list.get(list.size()-1) - list.get(0));
+		
+
 	}
 
 }
