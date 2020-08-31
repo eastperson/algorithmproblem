918ccfe63fcb54b6a05001c31ae2e686508a7db0
EP <kjuioqq8@gmail.com>
2020-08-31 23:38:01 +0900

    (2020.08.31) Baek algorithm sort

--- 백준알고리즘/sort/Problem2108.java
+++ 백준알고리즘/sort/Problem2108.java
@@ -2,8 +2,15 @@ package sort;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+class Value { int value = 0; }
 
 public class Problem2108 {
 	
@@ -11,10 +18,22 @@ public class Problem2108 {
 		// N은 홀수
 		Scanner sc = new Scanner(System.in);
 		List<Integer> list = new ArrayList<>();
+		Set<Integer> set = new TreeSet<>();
+		Map<Integer, Integer> map = new HashMap<>();
 		int sum = 0;
 		int n = sc.nextInt();
 		for(int i = 0; i < n; i ++) {
-			list.add(sc.nextInt());
+			int num = sc.nextInt();
+			list.add(num);
+			
+			if(!map.containsKey(num)) {
+				map.put(num, 1);
+			}
+			
+			else {
+				int value = (int) map.get(num);
+				map.put(num, value+1);
+			}
 		}
 		
 		Collections.sort(list);
@@ -24,7 +43,7 @@ public class Problem2108 {
 		}
 		
 		// 산술평균 : N개의 수들의 합을 N으로 나눈 값
-		System.out.println(sum/list.size());
+		System.out.println((int) (Math.round(sum/list.size())));
 		
 		
 		// 중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위차하는 값
@@ -32,8 +51,33 @@ public class Problem2108 {
 		System.out.println(list.get(a));
 		
 		// 최빈값 : N개의 수들 중 가장 많이 나타나는 값
-		Collections.
+		int max = 0;
+		int maxIdx = 0;
+		for(int i = 0; i < list.size(); i++) {
+			int idx = list.get(i);
+			if(max < map.get(idx)) {
+				max = map.get(idx);
+				maxIdx = idx;
+			}
+		}
 		
+		for(int i = 0; i < list.size(); i++) {
+			int idx = list.get(i);
+			if(max == map.get(idx)) {
+				set.add(idx);
+			}
+		}
+		
+		
+//		if(map.get(0) == map.get(1))
+//		
+//		
+		Iterator it = set.iterator();
+		it.next();
+		if(set.size() >= 2) 
+			System.out.println(it.next());
+		else
+			System.out.println(maxIdx);
 		
 		// 범위 : N개의 수들 중 최댓값과 최솟값의 차이
 		System.out.println(list.get(list.size()-1) - list.get(0));
