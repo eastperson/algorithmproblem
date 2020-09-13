0e21e53635fb5ae2b5006ebb6201d89cb2a0525b
EP <kjuioqq8@gmail.com>
2020-09-13 23:54:43 +0900

    (2020.09.13) Baek Algorithm Queue

---
+++ 백준알고리즘/queue/Problem18258.java
@@ -0,0 +1,47 @@
+package queue;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.DelayQueue;
+
+public class Problem18258 {
+	
+	public int check(String str,Queue queue) {
+		switch(str) {
+				
+		case "push" :
+			queue.add(e)
+			break;
+		case "pop" :
+			queue.poll();
+			break;
+		case "size" :
+			queue.size();
+			break;
+		case "empty" :
+			queue.isEmpty();
+			break;
+		case "front" :
+			queue.;
+			break;
+		case "back" :
+			break;
+			
+			
+		default :
+			break;
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Queue queue = new DelayQueue();
+		int n = Integer.parseInt(sc.nextLine());
+		for(int i = 0; i < n; i++) {
+			String str = sc.nextLine();
+			
+		}
+	}
+
+}
